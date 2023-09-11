70fcd1c657a8d6e951b4fbd00df12a5aac41c39d
joydozer <wuntujoy@gmail.com>
2023-09-11 19:26:05 +0800

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'metric_converter',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
